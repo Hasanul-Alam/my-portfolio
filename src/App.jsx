48eae09e1430bd38895e39d975790c5f84eb2054
Hasanul-Alam <superdev0000@gmail.com>
2024-07-30 12:21:49 +0600

    navbar and banner serction added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Banner from "./pages/Banner/Banner";
+import Navbar from "./pages/shared/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <div className="">
+        <Navbar></Navbar>
+        <Banner></Banner>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Banner/Banner.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Banner.css";
+import { ReactTyped } from "react-typed";
+
+const Banner = () => {
+  return (
+    <>
+      <section className="banner-section mt-[4rem]">
+        <div className="relative banner-image flex justify-center items-center">
+          <img
+            src="https://i.ibb.co/dp87ccH/hero-img.jpg"
+            alt="Hero Image"
+            className="opacity-40 object-cover w-full h-auto"
+          />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="text-center text-white">
+              <h1 className="text-7xl max-md:text-xl font-bold">
+                I am Hasanul Alam
+              </h1>
+              <p className="text-md md:text-3xl mt-2">
+                <ReactTyped
+                  backSpeed={50}
+                  strings={[
+                    "Web Developer",
+                    "Web Designer",
+                    "Freelancer",
+                  ]}
+                  typeSpeed={50}
+                  loop
+                />
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Banner;
