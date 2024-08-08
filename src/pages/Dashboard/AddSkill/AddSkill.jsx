59ce7680d75e0fe319b366fd1b8d679fee57f530
Hasanul-Alam <superdev0000@gmail.com>
2024-08-08 10:48:11 +0600

    trying to publish with axios

--- src/pages/Dashboard/AddSkill/AddSkill.jsx
+++ src/pages/Dashboard/AddSkill/AddSkill.jsx
@@ -1,11 +1,19 @@
-
-
+import { useEffect, useState } from "react";
+import { CiEdit } from "react-icons/ci";
+import { FaRegTrashCan } from "react-icons/fa6";
 const AddSkill = () => {
-    return (
-        <div>
-            <h2>This is add skill page.</h2>
-        </div>
-    );
+    const [skills, setSkills] = useState([]);
+    const handleEdit = (id) => {
+        console.log(id)
+    }
+    const handleDelete = (id) => {
+        console.log(id)
+    }
+  return (
+    <>
+      
+    </>
+  );
 };
 
 export default AddSkill;
